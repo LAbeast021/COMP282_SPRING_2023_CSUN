1a288d9a3ad7d4159221e83a907a68d57e0fb93e
LAbeast021 <kiarashhesampour@gmail.com>
2023-02-25 20:52:05 -0800

    first commit

---
+++ HesampourH1.java
@@ -0,0 +1,55 @@
+// CSUN SPRING 23   COMP 282 
+ // HesampourH1.java generate n random numbers from a to b and print the k numbers per line  
+ // Prepared by: Kiarash Hesampour, Feb 4, 23   
+  import java.util.*; 
+  import java.io.*;   
+  public class HesampourH1  { 
+
+    PrintStream prt = System.out; 
+
+    private void generate(int n, int a, int b, int k) {
+        int i , rand ;
+        prt.printf("\n");
+        for(i = 1; i <= n; i++) {
+             rand = (int) (Math.random() * (b - a)) + a;
+             prt.printf("\t %4d, " , rand);
+             if(i % k == 0){
+                 prt.printf("\n") ;
+             }
+                
+        }
+    }
+    public static void main(String args[]) throws Exception{ 
+        // declare local variables 
+        int n, a, b, k ;
+        int cnt = args.length; // get no. of atguments 
+      
+        //create an instance of a class 
+        HesampourH1 H1 = new HesampourH1();     
+      
+        // get no. of  arguments     
+        cnt = args.length;  
+                 
+        if (cnt < 4) {
+            System.out.printf("\n\n\tOOPS Invalid No. of aguments! EXIT.\n");
+            return;
+        }
+
+        n = Integer.parseInt(args[0]);
+        a = Integer.parseInt(args[1]);
+        b = Integer.parseInt(args[2]); 
+        k = Integer.parseInt(args[3]);
+
+        System.out.printf("\n\tINPUT: n = %4d, a = %4d, b = %4d, k = %4d", n, a, b, k);
+        if (a >= b){ // m should be < n
+            System.out.printf("\n\tSorry, a should be < b.\n");
+            return;
+            } // end if
+
+        H1.generate(n, a, b, k);
+         
+        //Pleas replace G. Dastghaibyfard with your name in next line     
+        System.out.printf("\n\n\tAuthor: K. Hesampour Date: %s\n", 
+        java.time.LocalDate.now());  
+      } // end main 
+    }
---
+++ HesampourH2.java
@@ -0,0 +1,117 @@
+// CSUN COMP 282 Spring 23, Hwk-2  HesampourH2.java
+// External Sorting - using Normal Sort
+// Author: G. Dastghaiby Fard
+
+import java.util.*;
+import java.io.*;
+
+public class HesampourH2{
+	
+    //use prt for System.out to save typing	
+	PrintStream prt = System.out;	
+	
+	// print file contents
+	private void prtfile(String fn){
+		//declare variables
+		int i = 0, x;
+		prt.printf("\n\t%s:", fn);
+	  try{
+	    Scanner inf = new Scanner(new File(fn)); 
+	    while (inf.hasNext()) {
+          //read an input from inf
+		  x = inf.nextInt();
+		  prt.printf("%4d ", x);
+		  i++;
+		  if(i % 16 == 0) 
+			  prt.printf("\n\t%s:", fn);
+	    } // endwhile
+		prt.printf("(count=%d)", i);
+		// close file
+		inf.close();
+      }catch (Exception e){prt.printf("\n\tRead Error! %s", e);}
+	} // end prtfile
+
+	// print file contents
+	private void prtfiles(String fn, int n){
+		int i;
+		prtfile(fn); // print input file
+		String fname ;
+		for (i = 1; i <= n; i++){
+			fname = "F" + i + ".txt";
+			prtfile(fname);
+		}
+	} // end prtfiles	
+
+	// SortNormal, with arrays of size M
+	private int SortNormal(String fn, int M){//regular sort
+		//Complete this Method
+	} // end SortNormal
+
+	private void swap(int a[], int i, int j){
+		int tmp = a[i];a[i] = a[j];a[j] = tmp;
+	} // end swap
+
+	private void heapsort(int a[], int n){
+		int m;
+		// convert to maxheap
+		for(m= n/2; m >= 1; m--) 
+			maxheapdn(a, i, n);	
+		//end for
+		
+		for(m= n; m > 1; m--){
+			swap(a, 1, m); 
+			maxheapdn(a, 1, m-1);
+		} //end for
+	} // end heapsort
+	
+	// maxheapdn(a[], int m, int n)
+	private void maxheapdn(int a[], int m, int n){
+		//Complete this Method
+	} // end maxheapdn(int a[], int m, int n)
+	
+   // Normal sort process method using arrays size M
+   private void process(String fn, int M){ 
+		int n;
+		prt.print("\n\tExternal Sorting Java program that:\n\t"+
+		"gets array size and inputfilename from \n\t"+
+		"program argument and creates one or more sorted files." +
+		"\n\t\tTo compile: javac HesampourH2.java" +
+		"\n\t\tTo execute: java  HesampourH2 M inputfilename"+
+		"\n\t\t   Example: java  HesampourH2 5 inputfilename");
+		
+		// Call SortNormal
+		n = SortNormal(fn, M);
+
+		//print input and output files
+		prtfiles(fn, n);	    
+   } //end process method
+				
+	public static void main(String[] args) throws Exception{
+		//get no. of arguments
+		int M, cnt = args.length; 
+		String fn;
+		if (cnt < 2){
+		    System.out.printf("\n\tOOOPS Invalid No. of arguments!"+
+			"\n\tTO Execute: java HesampourH2 5 input.txt");
+			return;
+		} // end if	
+			
+		// get largest array size
+		M = Integer.parseInt(args[0]);
+		
+		// get input file name
+		fn = args[1]; 
+			
+		System.out.printf("\n\tInput:%s, array size=%d", fn, M);
+			
+		// create an instance of HesampourH2 class
+		HesampourH2 srt = new HesampourH2();
+		
+		// call process method
+		srt.process(fn, M); 
+			
+		//Replace ????????? with your name in next line
+		System.out.printf("\n\n\tAuthor: ????????? Date: %s\n",
+		java.time.LocalDate.now());
+	}	// end main
+} // end HesampourH2
