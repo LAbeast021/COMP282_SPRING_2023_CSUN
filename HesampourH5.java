d97b412201039b6c5bd6ae78ee428cbee9a92c2e
LAbeast021 <kiarashhesampour@gmail.com>
2023-03-19 02:07:51 -0700

    h5 started 2

--- HesampourH5.java
+++ HesampourH5.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 // CSUN COMP 282 Spring 23, HesampourH5 .java
-// Program to merge 2 sorted files in one sorted file.
+// Program to merge k sorted files in one sorted file.
 // End of each inputfile is 2147483647(Integer.MAX_VALUE). 
 // Author: G. Dastghaiby Fard
 
@@ -32,70 +32,29 @@ public class HesampourH5 {
 			prt.printf("\nOoops! Read Exception: %s", e);}
 	} // end prtfile
 
-	private void merge2way(String fn1, String fn2, String fn3) throws FileNotFoundException{
-		int firstInt , secondInt ,  maxVal = Integer.MAX_VALUE ;
-		// int maxVal = 2147483647 ,
-		Scanner firstFile = new Scanner(new File(fn1));
-        Scanner secondFile = new Scanner(new File(fn2));
-        PrintWriter thirdFile = new PrintWriter(new File(fn3));
- 
-        firstInt = firstFile.nextInt();
-        secondInt = secondFile.nextInt();
+	private void mergeKway(String fn1, String fn2, String fn3) throws FileNotFoundException{
+		
+	} // end mergeKway
 
-		while (firstInt != maxVal && secondInt != maxVal) {
-			if(firstInt < secondInt) {
-				thirdFile.printf(" %3d " , firstInt);
-				firstInt = firstFile.nextInt() ;
-			}
-			else if (firstInt == secondInt) {
-				thirdFile.printf(" %3d  %3d" , firstInt , secondInt);
-				firstInt = firstFile.nextInt();
-				secondInt = secondFile.nextInt();
-			}
-			else if (firstInt > secondInt) {
-				thirdFile.printf(" %3d " , secondInt);
-				secondInt = secondFile.nextInt() ;
-			}
-		}
-		if (firstInt == maxVal) {
-			while (secondInt != maxVal){
-				thirdFile.printf(" %3d " , secondInt);
-				secondInt = secondFile.nextInt() ;
-			}
-		}
-		else if (secondInt == 2147483647) {
-			while (firstInt != 2147483647){
-				thirdFile.printf(" %3d " , firstInt);
-				firstInt = firstFile.nextInt() ;
-			}
-		}
+	public static void main(int[] args) throws Exception{
+		int cnt = args.length , k; // get no. of atguments
 
-			firstFile.close();
-            secondFile.close();
-            thirdFile.close();
-	} // end merge2way
-
-	public static void main(String[] args) throws Exception{
-		int cnt = args.length; // get no. of atguments
-		String fn1, fn2, fn3;
-		if (cnt < 3){
+		if (cnt < 1){
 		    System.out.printf("\n\tOOOPS Invalid No. of aguments!"+
 			"\n\tTO Execute: java HesampourH5  f1 f2 f3");
 			return;
 		} // end if	
 		// get input file name
-		fn1 = args[0]; 
-		fn2 = args[1]; 
-		fn3 = args[2]; 
+		k = args[0]; 
 
-		HesampourH5  srt = new HesampourH5 ();
+		HesampourH5  srt = new HesampourH5() ;
 		//Print input files
-		srt.prtfile(fn1);
-		srt.prtfile(fn2);
-		//Call merge2way
-		srt.merge2way(fn1, fn2, fn3);
-		//Print output file
-		srt.prtfile(fn3);
+		// srt.prtfile(fn1);
+		// srt.prtfile(fn2);
+		// //Call mergeKway
+		// srt.mergeKway(fn1, fn2, fn3);
+		// //Print output file
+		// srt.prtfile(fn3);
 		
 		
 		//Replace ????????? with your name in next line
